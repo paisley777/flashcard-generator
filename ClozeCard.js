e1899b0535e97cf2fe478cc0c85fbeaf0b9a63b0
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-07-30 17:35:52 -0500

    scope safe constructors

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,8 +1,23 @@
 var ClozeCard = function(text, cloze) {
+    //the 'if' statement makes this a scope-safe constructor
+    if (!(this instanceof ClozeCard)) { 
+        return new ClozeCard(text, cloze);
+    }
     this.fullText = text; 
-    this.cloze = cloze; 
+    this.cloze = cloze;
+    
     this.partial = function(text, cloze) {
-        text.replace(cloze, '....');   
+        //search for the cloze phrase in the text string
+        var brokenCloze = text.search(cloze);
+        //if there is no match, log an error message
+        if (brokenCloze === -1) {
+            var error = 'Error! The cloze phrase doesn\'t exist in the statement.';
+            return error;
+        //otherwise replace the cloze statement with a blank 
+        } else {
+            var newString = text.replace(cloze, '....');
+            return newString;
+        }  
     }
 }
 
--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
 
-var firstPresident = new BasicCard('Who was the first president of the United States?', 'George Washington');
-var firstPresidentCloze = new ClozeCard('George Washington was the first president of the United States.', 'George Washington');
+//constructors called without use of the word "new" keyword
+var firstPresident = BasicCard('Who was the first president of the United States?', 'George Washington');
+var firstPresidentCloze = ClozeCard('George Washington was the first president of the United States.', 'George Washington');
+var brokenClozeCard = ClozeCard('George Washington was the first president of the United States.', 'John Adams');
 
+console.log('\n-----This is a BASIC CARD-----')
 console.log('BASIC CARD FRONT: ' + firstPresident.front);
 console.log('BASIC CARD BACK: ' + firstPresident.back);
 
+console.log('\n-----This is a CLOZE CARD-----')
 console.log('CLOZE: ' + firstPresidentCloze.cloze);
-console.log('FULL TEXT: ' + firstPresidentCloze.fullText);
 console.log('PARTIAL TEXT: ' + firstPresidentCloze.partial(firstPresidentCloze.fullText, firstPresidentCloze.cloze));
+console.log('FULL TEXT: ' + firstPresidentCloze.fullText);
+
+console.log('\n-----This is a BROKEN CLOZE CARD-----')
+console.log('CLOZE: ' + brokenClozeCard.cloze);
+console.log('PARTIAL TEXT: ' + brokenClozeCard.partial(brokenClozeCard.fullText, brokenClozeCard.cloze));
+console.log('FULL TEXT: ' + brokenClozeCard.fullText);
+
